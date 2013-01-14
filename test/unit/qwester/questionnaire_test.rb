cf758a8828d6376b719623dfcfbb6f6df3b29611
Rob Nichols <git@nicholshayes.co.uk>
2013-01-14 09:07:15 +0000

    Tidies comments

--- test/unit/qwester/questionnaire_test.rb
+++ test/unit/qwester/questionnaire_test.rb
@@ -6,7 +6,8 @@ module Qwester
       @questionnaire = Questionnaire.find(1)
     end
 
-    # Overriding this method so want to make sure default behaviour still works
+    # Overriding this method, so want to make sure default behaviour still works
+    # Tests for methods over-riding this are in other test, e.g. questionnaires_question_test.rb
     def test_method_missing
       assert_raise NoMethodError do
         @questionnaire.no_such_method
