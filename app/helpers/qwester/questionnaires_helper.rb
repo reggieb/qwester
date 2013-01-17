3381167688b0af40bd4a1e02c9c551622c1b78f5
Rob Nichols <git@nicholshayes.co.uk>
2013-01-17 17:29:44 +0000

    Adds test that previous answers to a question are not passed on to next answer to that question
    With first attempts to fix and call to highlight problem

--- app/helpers/qwester/questionnaires_helper.rb
+++ app/helpers/qwester/questionnaires_helper.rb
@@ -19,11 +19,8 @@ module Qwester
     end
 
     def answer_store_answers
-      @answer_store_answers ||= get_answer_store_answers
-    end
-
-    def get_answer_store_answers
-      get_qwester_answer_store ? @qwester_answer_store.answers : []
+      answer_store = get_qwester_answer_store
+      answer_store ? answer_store.answers : []
     end
   end
 end
--- lib/rails/actionpack/lib/action_controller/base.rb
+++ lib/rails/actionpack/lib/action_controller/base.rb
@@ -3,16 +3,27 @@ module ActionController
     
     protected
     def update_qwester_answer_store
+      @qwester_answer_store = nil
       get_qwester_answer_store(true)
       add_answers_to_qwester_answer_store
       add_questionnaire_to_qwester_answer_store
+      @qwester_answer_store.save
+       p @qwester_answer_store.answers.collect &:id
+       p Qwester::AnswerStore.last.answers.collect &:id
     end
 
     def add_answers_to_qwester_answer_store
       answers = params[:question_id].values.collect do |question_values|
         question_values[:answer_ids].collect{|id| Qwester::Answer.find(id)}
       end
-      @qwester_answer_store.answers = (@qwester_answer_store.answers | answers.flatten)
+      answers.flatten!
+      question_ids = answers.collect(&:question_id).uniq
+      before =  @qwester_answer_store.answers.collect &:id
+      @qwester_answer_store.answers.delete_if {|a| question_ids.include? a.question_id.to_i}
+      
+      after = @qwester_answer_store.answers.collect &:id
+      puts "#{before} > #{after} #{answers.collect &:id}"
+      @qwester_answer_store.answers = (@qwester_answer_store.answers | answers)      
     end
 
     def add_questionnaire_to_qwester_answer_store
