c8337a5a93c79578cceb9e4a97b31d27b6158d0d
Rob Nichols <git@nicholshayes.co.uk>
2013-01-18 10:32:05 +0000

    Adds questionnaire seeds

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -3,6 +3,7 @@ Seeder = Dibber::Seeder
 
 Seeder.seeds_path = "#{Rails.root}/db/seeds"
 
+##############################
 Seeder.monitor AdminUser
 admin_email = 'admin@warwickshire.gov.uk'
 password = 'changeme'
@@ -12,4 +13,30 @@ AdminUser.create!(
   :password_confirmation => password
 ) unless AdminUser.exists?(:email => admin_email)
 
+##############################
+Seeder.monitor Qwester::Questionnaire
+Seeder.monitor Qwester::Question
+Seeder.monitor Qwester::Answer
+Seeder.monitor Qwester::RuleSet
+Seeder.process_log.start('first questionnaire questions', 'Qwester::Questionnaire.count > 0 ? Qwester::Questionnaire.first.questions.length : 0')
+
+Seeder.objects_from('questions.yml').each do |holder, data|
+  questionnaire = Qwester::Questionnaire.find_or_create_by_title(data['title'])
+
+  if data['questions']
+    data['questions'].values.each do |question_data|
+      question = Qwester::Question.find_or_create_by_title(question_data['question'])
+      question.create_standard_answers
+      answer = question.answers.find_or_create_by_value('Yes')
+      rule_set = Qwester::RuleSet.find_or_initialize_by_title(question_data['title'])
+      rule_set.url = question_data['url']
+      rule_set.answers = [answer]
+      rule_set.save
+      questionnaire.questions << question
+    end
+  end
+end
+
+
+##############################
 puts Seeder.report
