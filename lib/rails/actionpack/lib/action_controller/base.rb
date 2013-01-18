d32dfc300151d18d13225eb3bb635d6b9e52f1ed
Rob Nichols <git@nicholshayes.co.uk>
2013-01-18 11:34:27 +0000

    Refactor updating answer store so more reliable across many submissions

--- lib/rails/actionpack/lib/action_controller/base.rb
+++ lib/rails/actionpack/lib/action_controller/base.rb
@@ -3,27 +3,11 @@ module ActionController
     
     protected
     def update_qwester_answer_store
-      @qwester_answer_store = nil
       get_qwester_answer_store(true)
       add_answers_to_qwester_answer_store
       add_questionnaire_to_qwester_answer_store
-      @qwester_answer_store.save
     end
 
-    def add_answers_to_qwester_answer_store
-      answers = params[:question_id].values.collect do |question_values|
-        question_values[:answer_ids].collect{|id| Qwester::Answer.find(id)}
-      end
-      answers.flatten!
-      question_ids = answers.collect(&:question_id).uniq
-      @qwester_answer_store.answers.each {|a| @qwester_answer_store.answers.delete(a) if question_ids.include? a.question_id.to_i}
-      @qwester_answer_store.answers = (@qwester_answer_store.answers | answers)      
-    end
-
-    def add_questionnaire_to_qwester_answer_store
-      @qwester_answer_store.questionnaires << @questionnaire
-    end
-    
     def get_qwester_answer_store(create_new = false)
       if session[:qwester_answer_store]
         current_qwester_answer_store
@@ -44,12 +28,33 @@ module ActionController
     def set_qwester_answer_store(answer_store)
       @qwester_answer_store = answer_store
       session[:qwester_answer_store] = @qwester_answer_store.session_id
+      @qwester_answer_store
     end
     
     def matching_rule_sets
       if get_qwester_answer_store
         Qwester::RuleSet.matching(@qwester_answer_store.answers)
       end
+    end
+    
+#    private
+    def add_answers_to_qwester_answer_store
+      answers = params[:question_id].values.collect do |question_values|
+        question_values[:answer_ids].collect{|id| Qwester::Answer.find(id)}
+      end
+      answers.flatten!
+      remove_answers_to_questions_answered_with(answers) if answers.present?
+      @qwester_answer_store.answers = (@qwester_answer_store.answers | answers)      
+    end
+    
+    def remove_answers_to_questions_answered_with(answers)
+      question_ids = answers.collect(&:question_id).uniq
+      answers_to_delete = @qwester_answer_store.answers.collect {|a| a if question_ids.include? a.question_id.to_i}.compact
+      @qwester_answer_store.answers.delete(answers_to_delete)
+    end
+
+    def add_questionnaire_to_qwester_answer_store
+      @qwester_answer_store.questionnaires << @questionnaire
     end
   end
 end
