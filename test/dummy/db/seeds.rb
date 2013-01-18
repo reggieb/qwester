5193dea85365e9c8ea5b391fb54b4ec90474196e
Rob Nichols <git@nicholshayes.co.uk>
2013-01-18 10:37:04 +0000

    Prevents seeding repopulating questionnaires if questionnaires exist

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -20,19 +20,24 @@ Seeder.monitor Qwester::Answer
 Seeder.monitor Qwester::RuleSet
 Seeder.process_log.start('first questionnaire questions', 'Qwester::Questionnaire.count > 0 ? Qwester::Questionnaire.first.questions.length : 0')
 
-Seeder.objects_from('questions.yml').each do |holder, data|
-  questionnaire = Qwester::Questionnaire.find_or_create_by_title(data['title'])
+if Qwester::Questionnaire.count > 1
+  puts "**** Bypassing questionnaire, question, answer and rule set population"
+  puts "**** as there are already #{Qwester::Questionnaire.count} questionnaires in the system"
+else
+  Seeder.objects_from('questions.yml').each do |holder, data|
+    questionnaire = Qwester::Questionnaire.find_or_create_by_title(data['title'])
 
-  if data['questions']
-    data['questions'].values.each do |question_data|
-      question = Qwester::Question.find_or_create_by_title(question_data['question'])
-      question.create_standard_answers
-      answer = question.answers.find_or_create_by_value('Yes')
-      rule_set = Qwester::RuleSet.find_or_initialize_by_title(question_data['title'])
-      rule_set.url = question_data['url']
-      rule_set.answers = [answer]
-      rule_set.save
-      questionnaire.questions << question
+    if data['questions']
+      data['questions'].values.each do |question_data|
+        question = Qwester::Question.find_or_create_by_title(question_data['question'])
+        question.create_standard_answers
+        answer = question.answers.find_or_create_by_value('Yes')
+        rule_set = Qwester::RuleSet.find_or_initialize_by_title(question_data['title'])
+        rule_set.url = question_data['url']
+        rule_set.answers = [answer]
+        rule_set.save
+        questionnaire.questions << question
+      end
     end
   end
 end
