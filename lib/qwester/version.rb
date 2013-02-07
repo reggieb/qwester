002a8c74556a9085b4d92cf12d58d5d3e5de8b46
Rob Nichols <git@nicholshayes.co.uk>
2013-02-07 11:34:49 +0000

    Bump version so as to release bug fix.

--- lib/qwester/version.rb
+++ lib/qwester/version.rb
@@ -1,7 +1,11 @@
 module Qwester
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
 
-# 0.0.5 - minor update
+# 0.0.6 - bug fix update
+#   Fixes issue where error raised if user submits a questionnaire without
+#   selecting and answer.
+#
+# 0.0.5 - bug fix update
 #   Fixes issue where active admin rule set form did not include link text field.
 #
