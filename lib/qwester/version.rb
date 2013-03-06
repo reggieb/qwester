943667d497f0c115f79beba40ca1bb8e25330065
Rob Nichols <git@nicholshayes.co.uk>
2013-03-06 15:57:40 +0000

    Bump version to 0.0.8

--- lib/qwester/version.rb
+++ lib/qwester/version.rb
@@ -1,9 +1,13 @@
 module Qwester
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
 end
 
 # History
 # =======
+# 
+# 0.0.8 - add facility to preserve answer stores
+#   This allows snaps shots to be taken, and redundant answer stores to be 
+#   removed during routine maintenance.
 # 
 # 0.0.7 - improves deployment process
 #   Fixes a bug in one of the migrations and updates the README
