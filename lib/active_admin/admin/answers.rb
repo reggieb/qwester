065035146da188881199b7165d596ce713ba6bcf
Rob Nichols <git@nicholshayes.co.uk>
2013-04-02 11:34:45 +0100

    Updates active admin answers register to use weighting alias more widely

--- lib/active_admin/admin/answers.rb
+++ lib/active_admin/admin/answers.rb
@@ -8,6 +8,14 @@ module Qwester
 
     actions :all, :except => [:edit]
     config.batch_actions = false
+    
+    filter :question
+    filter :value
+    filter :created_at
+    filter :updated_at
+    filter :position
+    filter :weighting, :label => Answer.weighting_alias_name.humanize
+    
 
     index do
       column :id
@@ -15,9 +23,22 @@ module Qwester
       column 'Question (edit answer via question)', :question do |answer|
         link_to(answer.question.title, edit_admin_qwester_question_path(answer.question)) if answer.question
       end
-      column :position
       column Answer.weighting_alias_name.humanize, :weighting
+      column :position      
       default_actions
+    end
+    
+    show do |ad|
+      attributes_table do
+        row :question
+        row :value
+        row :updated_at
+        row :position
+        row Answer.weighting_alias_name.humanize do |answer|
+          answer.weighting
+        end
+      end
+      active_admin_comments
     end
     
     member_action :move_up do
